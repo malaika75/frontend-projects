4187576a1ff78729cef9d4af5e6d303eeae8ce8f
malaika75 <malaika57680@gmail.com>
2024-08-27 10:41:43 +0500

    projects

---
+++ amazon-clone/index.ts
@@ -0,0 +1,9 @@
+function menu(){
+    let sidebar = document.getElementById("side-bar");
+    sidebar.classList.toggle("open")
+}
+
+function toggleSubmenu(id) {
+    let submenu = document.querySelector(sub-menu1);
+    submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+}
